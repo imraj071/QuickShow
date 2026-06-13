b47546839fa6525dea7534fe58c837828dc7de20
imraj071 <imraj071@gmail.com>
2026-06-13 23:38:01 +0530

    vercel initialization

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
 
-// import dns from 'node:dns/promises'
-// dns.setServers(["1.1.1.1", "1.0.0.1"]);
+import dns from 'node:dns/promises'
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
 const dbName = "quickshow"
 
 const connectDB = async () => {
     try{
-        
-        await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`)
+        //console.log(`${process.env.MONGODB_URI}/${dbName}`)
         mongoose.connection.on('connected', () => console.log('Database connected'));
+        await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`)
         
 
     }catch(error){
