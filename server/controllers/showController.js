c3f21321f667eae14864fe025d819262c68ed8ba
imraj071 <imraj071@gmail.com>
2026-06-14 14:59:10 +0530

    Added APIs for getting a show or all shows

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -21,7 +21,6 @@ export const getNowPlayingMovies = async (req,res) =>{
 }
 
 //API to add a new show to the database
-
 export const addShow = async (req,res) => {
     try{
 
@@ -87,5 +86,57 @@ export const addShow = async (req,res) => {
     } catch(error){
         console.log(error)
         res.json({success:false, message: error.message})
+    }
+}
+
+//API to get all shows from the database
+export const getShows = async (req,res) => {
+    try{
+        const shows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie').sort({showDateTime:1});
+        
+        //filter unique shows
+        const uniqueShows = new Set(shows.map(show => show.movie))
+
+        // const uniqueShows = Object.values(
+        //     shows.reduce((acc, show) => {
+        //         acc[show.movie._id] = show.movie;
+        //             return acc;
+        //     }, {})
+        // );
+
+        res.json({success: true, shows: Array.from(uniqueShows)})
+
+    } catch(error){
+        console.error(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
+//API to get a single show from the database
+export const getShow = async (req,res) => {
+    try{
+
+        const {movieId} = req.params;
+
+        //get all upcoming shows for the movie
+        const shows = await Show.find({movie: movieId, showDateTime: { $gte: new Date() }})
+
+        const movie = await Movie.findById(movieId);
+        const dateTime = {};
+
+        shows.forEach((show)=> {
+            const date = show.showDateTime.toISOString().split("T")[0];
+            if(!dateTime[date]){
+                dateTime[date] = []
+            }
+
+            dateTime[date].push({time: show.showDateTime, showId: show._id})
+        })
+
+        res.json({success: true, movie,dateTime})
+
+    }catch(error){
+        console.error(error);
+        res.json({success: false, message: error.message})
     }
 }
