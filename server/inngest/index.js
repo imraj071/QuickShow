7166771d78ec22702d4645fe70b955d648ce4cab
imraj071 <imraj071@gmail.com>
2026-06-16 20:55:33 +0530

    update mail template

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -94,7 +94,7 @@ const sendBookingConfirmationEmail = inngest.createFunction(
 
         await sendEmail({
             to: booking.user.email,
-            subject: `Payment Confirmation "${booking.show.movie.title}" booked!`,
+            subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
             body: `<div style='font-family: Arial, sans-serif; line-height:1.5;'>
                     <h2>Hi ${booking.user.name},</h2>
                     <p>
