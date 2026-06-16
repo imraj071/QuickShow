73cadbaef97db8eaf7685d849cf22416064d65ee
imraj071 <imraj071@gmail.com>
2026-06-16 12:31:49 +0530

    Fixed Booking Schema

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
     user: {type: String, required: true, ref: 'User'},
-    shows: {type: String, required: true, ref: 'Show'},
+    show: {type: String, required: true, ref: 'Show'},
     amount: {type: Number, required: true},
     bookedSeats: {type: Array, required: true},
     isPaid: {type: Boolean, default: false},
