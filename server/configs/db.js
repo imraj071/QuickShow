1e4c8cd6c064643a989e6837fae71bbd75cc23d1
imraj071 <imraj071@gmail.com>
2026-06-16 23:20:28 +0530

    Updated db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,11 +5,22 @@ dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
 const dbName = "quickshow"
 
+let isConnected = false;
+
 const connectDB = async () => {
+
+    if(isConnected) return;
+
     try{
-        //console.log(`${process.env.MONGODB_URI}/${dbName}`)
+        
         mongoose.connection.on('connected', () => console.log('Database connected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`,{
+            serverSelectionTimeoutMS: 30000,
+            socketTimeoutMS: 45000,
+            bufferCommands: false,
+        })
+
+        isConnected = true;
         
 
     }catch(error){
