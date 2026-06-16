a9ab5d612885bf98f829c54b54c37e680b088a8e
imraj071 <imraj071@gmail.com>
2026-06-16 14:33:56 +0530

    Create Stripe Web Hooks

---
+++ server/controllers/stripeWebHooks.js
@@ -0,0 +1,51 @@
+import stripe from 'stripe';
+import Booking from '../models/Booking.js';
+
+export const stripeWebHooks = async (req,res) => {
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+    const sig = req.headerss["stripe-signature"]
+
+    let event;
+
+    try{
+
+        event = stripeInstance.webhooks.constructEvent(req.body,sig,process.env.STRIPEWEBHOOK_SECRET)
+
+    } catch(error){
+        return res.status(400).send(`Webhook Error: ${error.message}`)
+    }
+
+    try{
+
+        switch(event.type){
+            case "payment_intent.succeeded": {
+                const paymentIntent = event.data.object
+                const sessionList = await stripeInstance.checkout.sessions.list({
+                    payment_intent: payment_intent.id
+                })
+
+                const session = sessionList.data[0];
+                const {bookingId} = session.metadata;
+
+                await Booking.findByIdAndUpdate(bookingId, {
+                    isPaid: true,
+                    paymentLink: ""
+                })
+                break;
+            }
+
+            default:
+                console.log('Unhandled event type: ', event.type)
+
+        }
+        Response.json({received: true})
+
+    } catch(error){
+
+        console.log("Webhook processing error: ", err)
+        res.status(500).send('Internal Server Error');
+
+    }
+}
+
