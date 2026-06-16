fa4809e9d0aaea25611b919cdc64a6e7a6d82994
imraj071 <imraj071@gmail.com>
2026-06-16 23:47:35 +0530

    Update inngest function

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -49,7 +49,7 @@ const syncUserUpdation = inngest.createFunction(
         const userData = {
             _id: id,
             email: email_addresses[0].email_address,
-            name: first_name+last_name,
+            name: first_name+" "+last_name,
             image: image_url
         }
 
