62b7437f6ebf75e1d56753e6cce791231680091d
imraj071 <imraj071@gmail.com>
2026-06-16 13:01:30 +0530

    Initialized Stripe Payment Gateway in Booking Controller

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,6 @@
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js"
+import stripe from 'stripe'
 
 
 //Function to check availability of selected seats for a movie
@@ -59,8 +60,35 @@ export const createBooking = async (req,res) => {
         await showData.save()
 
         // Stripe Gateway Initialize
+        const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
 
-        res.json({success: true, message: 'Booked successfully'})
+        // Creating line items for Stripe
+        const line_items = [{
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: showData.movie.title
+                },
+                unit_amount: Math.floor(booking.amount) * 100
+            },
+            quantity : 1
+        }]
+
+        const session = await stripeInstance.checkout.sessions.create({
+            success_url: `${origin}/loading/my-bookings`,
+            cancel_url = `${origin}/my-bookings`,
+            line_items: line_items,
+            mode: "payment",
+            metadata: {
+                bookingId: booking._id.toString()
+            },
+            expires_at: Math.floor(Date.now()/1000) + 30*60, // Expires in 30 mins
+        })
+
+        booking.paymentLink = session.url
+        await booking.save()
+
+        res.json({success: true, url: session.url})
 
     } catch(error){
 
