be3a82c9d565c0ac05f250d816f59b63ac823c09
imraj071 <imraj071@gmail.com>
2026-06-16 14:15:43 +0530

    Fixed bookingController cancel_url bug

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -76,7 +76,7 @@ export const createBooking = async (req,res) => {
 
         const session = await stripeInstance.checkout.sessions.create({
             success_url: `${origin}/loading/my-bookings`,
-            cancel_url = `${origin}/my-bookings`,
+            cancel_url: `${origin}/my-bookings`,
             line_items: line_items,
             mode: "payment",
             metadata: {
