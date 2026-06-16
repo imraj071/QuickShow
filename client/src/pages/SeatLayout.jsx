58b40dfea822d607ada7c040335bb0105f2fbdd8
imraj071 <imraj071@gmail.com>
2026-06-16 12:32:33 +0530

    Updated Seat Layout page to handle Already Occupied Seats

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -17,6 +17,7 @@ const SeatLayout = () => {
   const [selectedSeats,setSelectedSeats] = useState([])
   const [selectedTime,setSelectedTime] = useState(null)
   const [show,setShow] = useState(null)
+  const [occupiedSeats, setOccupiedSeats] = useState([]);
 
   const navigate = useNavigate()
 
@@ -44,6 +45,9 @@ const SeatLayout = () => {
     if(!selectedSeats.includes(seatId) && selectedSeats.length>4){
       return toast("You can only select 5 seats")
     }
+    if(occupiedSeats.includes(seatId)){
+      return toast('This seat is already booked')
+    }
     setSelectedSeats(prev => prev.includes(seatId) ? prev.filter(seat => seat!== seatId) : [...prev, seatId])
   }
 
@@ -53,7 +57,7 @@ const SeatLayout = () => {
         {Array.from({length:count}, (_,i) => {
           const seatId = `${row}${i+1}`;
           return (
-            <button key={seatId} onClick={()=> handleSeatClick(seatId)} className={`h-8 w-8 rounded border border-primary/60 cursor-pointer ${selectedSeats.includes(seatId) && "bg-primary text-white"}`}>
+            <button key={seatId} onClick={()=> handleSeatClick(seatId)} className={`h-8 w-8 rounded border border-primary/60 cursor-pointer ${selectedSeats.includes(seatId) && "bg-primary text-white"} ${occupiedSeats.includes(seatId) && 'opacity-50'}`}>
               {seatId}
             </button>
           );
@@ -64,9 +68,56 @@ const SeatLayout = () => {
     </div>
   )
 
+  const getOccupiedSeats = async () => {
+    try{
+
+      const {data} = await axios.get(`/api/bookings/seats/seats/${selectedTime.showId}`)
+
+      if(data.success){
+        setOccupiedSeats(data.occupiedSeats)
+      }else{
+        toast.error(data.message);
+      }
+
+    } catch(error){
+      console.log(error)
+    }
+  }
+
+  const bookTickets = async() => {
+    try{
+
+      if(!user){
+        return toast.error("Please Login to proceed")
+      }
+
+      if(!selectedTime || !selectedSeats.length){
+        return toast.error('Please select a time and seat');
+      }
+
+      const {data} = await axios.post('/api/booking/create', {showId: selectedTime.showId, selectedSeats}, {headers: {Authorization: `Bearer ${await getToken()}`}});
+
+      if(data.success){
+        toast.success(data.message);
+        navigate('/my-bookings')
+      } else{
+        toast.error(data.message)
+      }
+
+    } catch(error){
+      toast.error(error.message)
+    }
+  }
+
   useEffect(()=>{
     getShow()
   }, [])
+
+  useEffect(()=>{
+    if(selectedTime){
+      getOccupiedSeats()
+    }
+  },[selectedTime])
   
   return show ? (
     <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50'>
@@ -104,7 +155,7 @@ const SeatLayout = () => {
             </div>
             </div>
 
-            <button onClick={() => navigate('/my-bookings')} className='flex items-center gap-1 mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer active:scale-95'>
+            <button onClick={bookTickets} className='flex items-center gap-1 mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer active:scale-95'>
               Proceed to Checkout
               <ArrowRightIcon strokeWidth={3} className='w-4 h-4'/>
             </button>
